e32b36d4e12642b76c8c193c4389aaaf71912966
Futsiph <151166188+Futsiph@users.noreply.github.com>
2026-06-26 15:17:49 +0300

    Update yolo_training.py

--- src/train/yolo_training.py
+++ src/train/yolo_training.py
@@ -1,14 +1,17 @@
 from ultralytics import YOLO
 
 def train():
-    model = YOLO('models/best.pt')
+    model = YOLO('runs/detect/circuit_object-7/weights/best.pt')
     model.to('cuda')
     result = model.train(
-        data = "data/dataset/data.yaml",
+        data = "data/dataset2/data.yaml",
         epochs = 50,
         batch = 16,
         name = "circuit_object",
-        save_period = 5  # Save a checkpoint every 5 epochs
+        save_period = 5,
+        workers = 4,
+        exist_ok = True,
+        pretrained = True
     )
 
 if __name__ == '__main__':
